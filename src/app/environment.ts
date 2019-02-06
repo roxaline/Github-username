59b1d808957e9af404f5798749b8bf5037db2985
roxaline <mariadelarosita078@gmail.com>
2019-02-06 13:42:57 +0200

    insertion of a token

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,2 +1,5 @@
-export class Environment {
-}
+export const environment = {
+    production: false,
+    apiUrl:"http://api.github.com/users/",
+    apikey: "?access_token=ad007205b2b21f746724bc59b603a78faa10a099"
+  };
