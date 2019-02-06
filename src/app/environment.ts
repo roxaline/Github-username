ec1051fdd8161c286edb45806df8c59d38ebffcb
roxaline <mariadelarosita078@gmail.com>
2019-02-06 14:03:45 +0200

    insertion of element in github-form

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,3 @@
-export const environment = {
-    production: false,
-    apiUrl:"http://api.github.com/users/",
-    apikey: "?access_token=ad007205b2b21f746724bc59b603a78faa10a099"
-  };
+export class Environment {
+    
+}
